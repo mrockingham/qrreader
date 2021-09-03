7c45fba82af95403f1026140a6bfb18cf6016d48
Michael Rockingham <mrockingham2@gmail.com>
2021-09-02 20:36:23 -0400

    qr reader

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+type CameraList = {
+    id?: String;
+    label?: String;
+
+}[]
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable array-callback-return */
+import { useState, useEffect } from 'react';
+import { Html5Qrcode } from '@cosva-lab/html5-qrcode';
+
+import {
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+} from '@chakra-ui/react';
 import './App.css';
+import { idText } from 'typescript';
+
+// function onScanSuccess(decodedText: any, decodedResult: any) {
+//   // Handle on success condition with the decoded text or result.
+//   console.log(`Scan result: ${decodedText}`, decodedResult);
+// }
+// function onScanError(errorMessage: any) {
+//   // handle on error condition, with error message
+//   console.log(errorMessage);
+// }
+
+// var html5QrcodeScanner = new Html5QrcodeScanner('reader', {
+//   fps: 10,
+//   qrbox: 250,
+// });
+// html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+interface CameraProps {
+  CamName: CameraList;
+}
 
 function App() {
+  const [camLabel, setCamLabel] = useState<CameraList>([]);
+  const [camMessage, setCamMessage] = useState('');
+  const [camError, setCamError] = useState('');
+
+  const openQRScanner = () => {
+    Html5Qrcode.getCameras()
+      .then(devices => {
+        setCamLabel(devices);
+        if (devices && devices.length) {
+          var cameraId = devices[0].id;
+          // .. use this to start scanning.
+        }
+      })
+      .catch(err => {
+        // handle err
+      });
+  };
+
+  useEffect(() => {
+    const html5QrCode = new Html5Qrcode('reader');
+
+    const starts = Html5Qrcode.getCameras().then(devices => {
+      setCamLabel(devices);
+      if (devices && devices.length) {
+        var cameraId = devices[0].id;
+        const html5QrCode = new Html5Qrcode(/* element id */ 'reader');
+        html5QrCode
+          .start(
+            cameraId,
+            {
+              fps: 10, // Optional frame per seconds for qr code scanning
+              qrbox: 250, // Optional if you want bounded box UI
+            },
+            qrCodeMessage => {
+              console.log(qrCodeMessage);
+              setCamMessage(qrCodeMessage);
+            },
+            errorMessage => {
+              // console.log(errorMessage);
+              setCamError(errorMessage);
+            }
+          )
+          .catch(err => {
+            // Start failed, handle it.
+          });
+      }
+    });
+    {
+    }
+    console.log('starting', starts);
+  }, []);
+
+  // console.log('starting', starts);
+  console.log('labels', camLabel);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Something
+      <div id="reader"></div>
+      <Text>Select Camera</Text>
+      <Button colorScheme="teal" size="md" onClick={openQRScanner}>
+        OpenQR
+      </Button>
+      <Menu>
+        {({ isOpen }) => (
+          <>
+            <MenuButton size="lg" isActive={isOpen} as={Button}>
+              {isOpen ? 'Close' : 'Open'}
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={() => alert('Kagebunshin')}>pizza</MenuItem>
+              <MenuItem onClick={() => alert('Kagebunshin')}>pizza</MenuItem>
+              {camLabel.map(camera => (
+                <MenuItem key={+camera.id} onClick={() => alert('Kagebunshin')}>
+                  {camera.label} pizza
+                </MenuItem>
+              ))}
+            </MenuList>
+          </>
+        )}
+      </Menu>
+      <div>cam:message {camMessage}</div>
+      <div>cam:error{camError}</div>
+      <div id="reader2"></div>
     </div>
   );
 }
 
 export default App;
+
+// <Menu>
+//   {({ isOpen }) => (
+//     <>
+//       <MenuButton isActive={isOpen} as={Button}>
+//         {isOpen ? 'Close' : 'Open'}
+//       </MenuButton>
+//       <MenuList>
+//         {/* {camLabel.map(camera => {
+//           <div>
+//           </div>;
+//         })} */}
+//         <MenuItem onClick={() => alert('Kagebunshin')}>
+//           {/* {camera.label} */}pi
+//         </MenuItem>
+//         <MenuItem onClick={() => alert('Kagebunshin')}>
+//           {/* {camera.label} */}pi
+//         </MenuItem>
+//       </MenuList>
+//     </>
+//   )}
+// </Menu>
